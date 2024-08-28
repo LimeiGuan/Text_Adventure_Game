f7739c57a10ebba73d0910fe5a79f8ac7c6c491d
Esen <esenharbinger@gmail.com>
2024-08-28 18:08:46 +0200

    added the Jackson integration to Inventory

--- the-hospital/src/main/java/hospital/Inventory.java
+++ the-hospital/src/main/java/hospital/Inventory.java
@@ -2,15 +2,46 @@ package hospital;
 
 
 import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The Inventory class manages a collection of items.
+ * It provides methods to add, remove, and search for items within the inventory.
+ */
 public class Inventory
 {
+    /** The list that holds the items in the inventory. */
 	private ArrayList<Item> inventory = new ArrayList<>();
 
+    /**
+     * Default constructor 
+     */
+	public Inventory() {}
+	/**
+	 * Constructor for deserialization.
+	 */
+    @JsonCreator
+    public Inventory(@JsonProperty("inventory") ArrayList<Item> inventory)
+    {
+        this.inventory = inventory != null ? inventory : new ArrayList<>();
+    }
+    
+    /**
+     * Adds an item to the inventory.
+     * 
+     * @param item the item to be added to the inventory
+     */
 	public void addItem(Item item)
 	{
 		inventory.add(item);
 	}
+	
+    /**
+     * Removes an item from the inventory by its name.
+     * 
+     * @param item the name of the item to be removed (case-insensitive)
+     */
 	public void removeItem(String item)
 	{
 		for(int i=0;i<inventory.size();i++)
@@ -22,6 +53,13 @@ public class Inventory
 			}
 		}
 	}
+	
+    /**
+     * Searches for an item in the inventory by its name.
+     * 
+     * @param item the name of the item to search for (case-insensitive)
+     * @return true if the item is found, false otherwise
+     */
 	public boolean searchItem(String item)
 	{
 		for(int i=0;i<inventory.size();i++)
@@ -31,5 +69,16 @@ public class Inventory
 		}
 		return false;
 	}
+	
+    /**
+     * Gets the list of items in the inventory.
+     * 
+     * @return the list of items
+     */
+    @JsonProperty("inventory")
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
 }
 
